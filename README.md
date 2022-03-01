fcface612dd206963289bc1bafe607cfb1686571
chenxu10 <shenxu2011@gmail.com>
2022-03-01 16:08:06 -0500

    docker setup

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# zixiu
-A search engine makes you become an expert 
+# ZiXiu
+A search engine makes you become an expert.
+
+## What's this project all about?
+
+This project showcases how to build a vertical search engine to help yourself become an expert in the area you want. It focuses on collecting research papers, free open courses, public textbooks, high-quality book and is now ads-free.
+
--- corealgo/pagerank.py
+++ corealgo/pagerank.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import numpy as np
 import matplotlib.pyplot as plt
 
 def pagerank():
@@ -28,18 +29,32 @@ def pagerank():
     return pagerank_scipy(G, alpha, personalization, max_iter, tol, nstart, weight, dangling)
 
 
-def pagerank_scipy(G, alpha, personalization, max):
+def pagerank_scipy(G, alpha=0.05, personalization=None, max_iter=100, tol=1.0e6, weight="weight"):
     """
 
     
     """
     # edge cases
-
-
     # convert G graph into a numpy sparse matrix
-    
-    
-    raise NotImplemented
+    # init a vector
+    # personalization
+    # power iteration
+
+    # if len(G) == 0:
+    #     return {}
+
+    # nodelist = list(G)
+    # M = nx.to_scipy_sparse_matrix(G, nodelist=nodelist, weight=weight, dtype=float)
+    # S = np.array(M.sum(axis=1)).flatten()
+    # return S
+
+
+
+
+    # choose start vector
+
+
+    # power_iteration
 
 
 if __name__ == "__main__":
@@ -51,9 +66,11 @@ if __name__ == "__main__":
         ("B","D",0.5),
         ("B","A",0.5),
         ("C","A",1),
-        ("D","C",0.5)])
+        ("D","C",0.5),
+        ("D","B",0.5)])
     nx.draw(DG, with_labels=True)
     # plt.title(r'$G({},{})$'.format(n,p))
     plt.show()
 
-    pagerank_scipy(DG)
+    S = pagerank_scipy(DG)
+    print(S)
